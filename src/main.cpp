e1291d8c28504b48ac0f2eefcdc4fb37a4caa1d1
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-12-03 22:20:14 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,22 @@
 
 static int output_pins[] = {LED_PIN /* led pin */, PB3, PB4};
 
+const uint32_t clock_ms = 100;
+uint32_t interval_ms = 5000;
+uint32_t activity_time_ms = 1000;
+uint32_t slept_ms = 0;
+uint32_t previous_tick_ms = 0;
+
+bool status = false;
+
+void toggle()
+{
+  status = !status;
+  digitalWrite(LED_PIN, status);
+  digitalWrite(PB3, status);
+  digitalWrite(PB4, !status);
+}
+
 void setup()
 {
   for (int pin : output_pins)
@@ -14,38 +30,29 @@ void setup()
   }
   pinMode(CUSTOM_BUTTON_PIN, INPUT);
   pinMode(ONBOARD_BUTTON_PIN, INPUT);
+  toggle();
 }
 
-uint32_t interval_ms = 1000;
-uint32_t interval2_ms = 1000;
-uint32_t slept_ms = 0;
-uint32_t clock_ms = 100;
-uint32_t status = 0;
-
-void toggle()
-{
-  for (int pin : output_pins)
-  {
-    digitalWrite(pin, pin % 2 ? status : !status);
-  }
-  status = !status;
-}
 
 void loop()
 {
   if (digitalRead(ONBOARD_BUTTON_PIN) == HIGH)
   {
-    interval_ms += clock_ms;
+    interval_ms += 2*clock_ms;
   }
   if (digitalRead(CUSTOM_BUTTON_PIN) == HIGH)
   {
-    interval2_ms += clock_ms;
+    activity_time_ms += 2*clock_ms;
   }
 
+  // This number will overflow in 50 days
   delay(clock_ms);
-  slept_ms += clock_ms;
+  uint32_t now = millis();
+  slept_ms += (now - previous_tick_ms);
+  previous_tick_ms = now;
 
-  if ((!status && slept_ms > interval_ms) || (status && slept_ms > interval2_ms))
+  if ((!status && (slept_ms > interval_ms))
+      || (status && (slept_ms > activity_time_ms)))
   {
     toggle();
     slept_ms = 0;
